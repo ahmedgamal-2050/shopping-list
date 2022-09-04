78b68d12ce52815f5311b24061a26cbc2be39033
Ahmed Gamal <ahmed.gamal@kickstartinteractive.net>
2022-09-04 16:19:43 +0200

    create project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/public/login/login.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { ShoppingComponent } from './components/public/shopping/container/shopping.component';
+import { RecipeComponent } from './components/public/recipe/container/recipe.component';
+import { RecipeDetailsComponent } from './components/public/recipe/components/recipe-details/recipe-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'shopping', component: ShoppingComponent },
+  { path: 'recipe', component: RecipeComponent },
+  { path: 'recipe-details', component: RecipeDetailsComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/public/login/login.component';
+import { HomeComponent } from './components/public/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard/dashboard.component';
+import { ShoppingComponent } from './components/public/shopping/container/shopping.component';
+import { ShoppingListComponent } from './components/public/shopping/components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './components/public/shopping/components/shopping-edit/shopping-edit.component';
+import { RecipeComponent } from './components/public/recipe/container/recipe.component';
+import { RecipeListComponent } from './components/public/recipe/components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './components/public/recipe/components/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './components/public/recipe/components/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    DashboardComponent,
+    ShoppingComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailsComponent,
+    RecipeComponent
   ],
   imports: [
     BrowserModule,
