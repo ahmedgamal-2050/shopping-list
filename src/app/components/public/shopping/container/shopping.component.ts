fbed98b691d5f10e8d899edf04644eca360bd08c
Ahmed Gamal <ahmed.gamal@kickstartinteractive.net>
2022-09-14 16:50:28 +0200

    add data

--- src/app/components/public/shopping/container/shopping.component.ts
+++ src/app/components/public/shopping/container/shopping.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping.component.scss']
 })
 export class ShoppingComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 
   constructor() { }
 
